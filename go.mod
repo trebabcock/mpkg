889ce0aa3c2c80ceb18339d3ed626277f7166882
tre.babcock@gmail.com <tre.babcock@gmail.com>
2021-09-09 23:07:28 -0500

    you can almost actually install something

---
+++ go.mod
@@ -0,0 +1,20 @@
+module mpkg
+
+go 1.17
+
+require (
+	github.com/gosuri/uitable v0.0.4
+	github.com/mingrammer/cfmt v1.1.0
+)
+
+require (
+	github.com/fatih/color v1.6.0 // indirect
+	github.com/kelindar/binary v1.0.13 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31 // indirect
+	golang.org/x/sys v0.0.0-20180315095008-cc7307a45468 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"os"
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"mpkg/pkg"
+	"mpkg/utils"
+	"os"
+	"os/exec"
+	"strings"
 
-	"github.com/mingrammer/cfmt"
 	"github.com/gosuri/uitable"
+	"github.com/kelindar/binary"
+	"github.com/mingrammer/cfmt"
 )
 
 type Command struct {
-	Name	string
-	Desc	string
-	Func	func()
+	Name string
+	Desc string
+	Func func()
 }
 
 var Commands = make(map[string]Command)
@@ -52,10 +60,58 @@ func setCommands() {
 		Func: Search,
 	}
 
+	info := Command{
+		Name: "info",
+		Desc: "Get information about a package",
+		Func: Info,
+	}
+
 	Commands["help"] = help
 	Commands["install"] = install
 	Commands["remove"] = remove
 	Commands["search"] = search
+	Commands["info"] = info
+}
+
+func Info() {
+	if len(os.Args) > 2 {
+		if strings.HasPrefix(os.Args[2], "./") {
+			infoLocal(os.Args)
+			return
+		}
+	}
+}
+
+func infoLocal(args []string) {
+	file, err := os.Open(args[2])
+	if err != nil {
+		cfmt.Errorln("Unable to open ", args[2])
+		os.Exit(1)
+	}
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		cfmt.Errorln("Unable to read ", args[2])
+		os.Exit(1)
+	}
+
+	mpkg := pkg.Package{}
+
+	binary.Unmarshal(fileBytes, &mpkg)
+
+	fmt.Printf("mpkg Version: %s\n", mpkg.Header.MpkgVersion)
+	fmt.Printf("Package Name: %s\n", mpkg.Meta.Name)
+	fmt.Printf("Package Version: %s\n", mpkg.Meta.Version)
+	fmt.Println("Package Dependencies:")
+	for _, d := range mpkg.Meta.Dependencies {
+		fmt.Printf("\t%s\n", d)
+	}
+	fmt.Println("Package Install Commands:")
+	for _, s := range mpkg.Meta.Scripts {
+		fmt.Printf("\t%s\n", s)
+	}
+	cs := len(mpkg.Content.Files)
+	fmt.Printf("Package Content Size: %d bytes\n", cs)
 }
 
 func Usage() {
@@ -75,7 +131,7 @@ func Usage() {
 func Search() {
 	if len(os.Args) > 2 {
 		if packageExists(os.Args[2]) {
-			cfmt.Infoln("Found package '"+os.Args[2]+"' in repo")
+			cfmt.Infoln("Found package '" + os.Args[2] + "' in repo")
 		} else {
 			cfmt.Warningln("Unable to find pacakeg '" + os.Args[2] + "' in repo")
 		}
@@ -83,14 +139,81 @@ func Search() {
 }
 
 func Install() {
+	//reader := bufio.NewReader(os.Stdin)
 	if len(os.Args) > 2 {
-		if packageExists(os.Args[2]) && !packageInstalled(os.Args[2]) {
-			cfmt.Successln("Successfully installed: " + os.Args[2])
-		} else if packageInstalled(os.Args[2]) {
-			
+		if strings.HasPrefix(os.Args[2], "./") {
+			installLocal(os.Args)
+			return
 		}
+		/*if packageExists(os.Args[2]) && !packageInstalled(os.Args[2]) {
+			fmt.Print("Are you sure you want to install ", os.Args[2], "? ", chalk.Magenta.Color("(yes, no) "))
+			response, _ := reader.ReadString('\n')
+			switch response {
+			case "y":
+				installPackage(os.Args[2])
+			case "yes\n":
+				installPackage(os.Args[2])
+			case "n":
+				os.Exit(0)
+			case "no":
+				os.Exit(0)
+			default:
+				os.Exit(0)
+			}
+			//
+		} else if packageInstalled(os.Args[2]) {
+
+		}*/
 	} else {
 		installHelp()
+	}
+}
+
+func installLocal(args []string) {
+	file, err := os.Open(args[2])
+	if err != nil {
+		cfmt.Errorln("Unable to open ", args[2])
+		os.Exit(1)
+	}
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		cfmt.Errorln("Unable to read ", args[2])
+		os.Exit(1)
+	}
+
+	mpkg := pkg.Package{}
+
+	binary.Unmarshal(fileBytes, &mpkg)
+
+	installPackage(&mpkg)
+}
+
+func installPackage(mpkg *pkg.Package) {
+	//cfmt.Successln("Successfully installed: " + packageName)
+	tarBall, err := os.Create("content.tar")
+	if err != nil {
+		cfmt.Errorln("Unable to create content.tar: ", err)
+		os.Exit(1)
+	}
+
+	tarBytes := bytes.NewReader(mpkg.Content.Files)
+
+	if _, err := io.Copy(tarBall, tarBytes); err != nil {
+		cfmt.Errorln("Unable to write to content.tar: ", err)
+		os.Exit(1)
+	}
+
+	utils.UntarFile("content.tar")
+
+	for _, c := range mpkg.Meta.Scripts {
+		cmd := exec.Command("bash", "-c", c)
+		stdout, err := cmd.Output()
+		if err != nil {
+			cfmt.Errorln("Unable to run Setup commands: ", err)
+			os.Exit(1)
+		}
+		fmt.Println(string(stdout))
 	}
 }
 
@@ -99,7 +222,7 @@ func installHelp() {
 }
 
 func Remove() {
-	
+
 }
 
 func removeHelp() {
